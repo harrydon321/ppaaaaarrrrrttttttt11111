538822dd834db6f4e6a2f936e50275d74064a7e9
sweetson <harpjosan@gmail.com>
2021-06-24 22:08:42 -0700

    Initial commit

---
+++ jijij.py
@@ -0,0 +1,27 @@
+character_name="josan"
+character_age="20"
+print("my name is" + character_name +"")
+print("he was"+ character_age + "")
+phrase="henry"
+print("hello kida \n ava")
+print("hello/jazzynn")
+phrase="baluu don badmash"
+print(phrase+ " is my freind")
+print(phrase.replace("baluu","kallu"))
+my_num=12
+print(str(my_num)+"itsa coll number")
+print(abs(my_num))
+print(pow(7,9))
+print((round(6.7)))
+from math import*
+print(floor(9.7))
+print(ceil(7.1))
+print(sqrt(36))
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("heloo " + name + "!")
+print("nice age " + age + "!")
+num1=input("enter your first number:")
+num2=input("enter your second number:")
+result=float(num1) + float(num2)
+print(result)
